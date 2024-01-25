c65b76344bf4f88ad46a4402e11ea248190032d2
Asitha Navaratne <asithan476@gmail.com>
2024-01-25 22:12:22 +0530

    Route to add appointments

--- backend/errors/data_exceptions.py
+++ backend/errors/data_exceptions.py
@@ -4,12 +4,24 @@ from starlette import status
 
 invalid_time_range_exception = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail='Start time cannot be past the ending time.',
+    detail='Start time cannot be past the ending time. Please choose an ending time past the specified start time',
     headers={"WWW-Authenticate": "Bearer"},
 )
 
 time_slot_conflict_exception = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail='Specified time slot overlaps with an existing availability slot.',
+    detail='Specified time slot overlaps with an existing availability slot. Please choose a time period that has not already been booked.',
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+time_not_available_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail='Specified doctor is not available at the given time. Please choose another time slot.',
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+appointments_exceeded_exception = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail='Specified doctor has exceeded the number of appointments for the given time. Please choose another time slot.',
     headers={"WWW-Authenticate": "Bearer"},
 )
--- backend/routers/appointments.py
+++ backend/routers/appointments.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
+from datetime import datetime
 from typing import Annotated
+from pydantic import BaseModel
 from starlette import status
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from .auth import get_current_user
+from database.models import Appointment, Availability
 from database.config import SessionLocal
+
+from errors.auth_exceptions import authorization_exception
+from errors.data_exceptions import time_not_available_exception, appointments_exceeded_exception
 
 
 def get_db():
@@ -22,6 +29,47 @@ router = APIRouter(
     tags=['Appointments']
 )
 
+# Types
+class CreateAppointmentRequest(BaseModel):
+    doctor_id: int
+    appointment_dttm: datetime
+
+# Routes
 @router.get('/')
 async def get_all_appointments(token: token_dependency, db: db_dependency):
-    return {'appointments': []}
+    if token['role'] == 'doctor':
+        appointments = db.query(Appointment).filter(Appointment.doctor_id == token['id']).all()
+    else:
+        appointments = db.query(Appointment).filter(Appointment.user_id == token['id']).all()
+
+    return {'appointments': appointments}
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def add_appointment(token: token_dependency, db: db_dependency, create_appointment_request: CreateAppointmentRequest):
+    if token['role'] != 'user':
+        raise authorization_exception
+    
+    time_available = db.query(Availability).filter(Availability.start_time <= create_appointment_request.appointment_dttm, Availability.end_time > create_appointment_request.appointment_dttm, Availability.doctor_id == create_appointment_request.doctor_id).first()
+
+    if not time_available:
+        raise time_not_available_exception
+    
+    existing_appointments = db.query(Appointment).filter(Appointment.appointment_dttm >= time_available.start_time, Appointment.appointment_dttm <= time_available.end_time, Appointment.doctor_id == create_appointment_request.doctor_id).all()
+
+    if len(existing_appointments) >= 10:
+        raise appointments_exceeded_exception
+
+    create_appointment_model = Appointment(
+        user_id = token['id'],
+        doctor_id = create_appointment_request.doctor_id,
+        appointment_dttm = create_appointment_request.appointment_dttm,
+        created_dttm = datetime.now(),
+    )
+
+    db.add(create_appointment_model)
+    db.commit()
+
+    return {
+        'number': len(existing_appointments) + 1,
+        'time': create_appointment_request.appointment_dttm
+    }
