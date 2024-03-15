f839b084185ae1557063e593b779eabc28a643d6
Asitha Navaratne <asithan476@gmail.com>
2024-03-15 12:24:25 +0530

    Added styles for Homepage component

--- frontend/src/views/HomePage/HomePage.tsx
+++ frontend/src/views/HomePage/HomePage.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
+import { Box } from "@mui/material";
+
+import styles from "./HomePage.module.scss";
+
+import Calendar from "../../components/Calendar/Calendar";
 
 const HomePage: FC = () => {
-  return <div></div>;
+  return (
+    <Box className={styles["home-page"]}>
+      <Box className={styles["home-page__calendar-section"]}>
+        <Calendar />
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
