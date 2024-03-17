e94b0d7b21001f79f8b51cfe1e2d5f1d9543f2ad
Asitha Navaratne <asithan476@gmail.com>
2024-03-17 23:27:14 +0530

    Using SCSS variables in the project - Started using SCSS variables in the project - Changed index.css to index.scss

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/pacifico";
-import "./index.css";
+import "./index.scss";
 
 import theme from "./styles/theme.ts";
 
